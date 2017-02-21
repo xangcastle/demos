ad057e91a64ba20ba6abdb1320bd473e8bca6056
Abel <abel@toruk>
2017-02-21 11:55:14 -0600

    cambio de base de datos

--- demos/settings.py
+++ demos/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'demos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'demos',
+    'USER': 'postgres',
+    'PASSWORD': 'postgres',
+    'HOST': 'localhost',
+    'PORT': '5432',
+},
 }
 
 
