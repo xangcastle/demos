03459a13484035389bf085796a0162105c8d01b8
Jose Williams Garcia <jwgarcia003@gmail.com>
2017-06-02 14:38:38 -0600

    fecha nacimiento usuario arca

--- control/views.py
+++ control/views.py
@@ -36,36 +36,36 @@ def actualizar_inventario():
     assert response.status_code == 200
     json_data = response.json()
     i = 321
+    if json_data:
+        for d in json_data:
+            i += 1
 
-    for d in json_data:
-        i += 1
+            if not d["producto_existencia"]:
+                existencia = 0
+            else:
+                existencia = d["producto_existencia"]
 
-        if not d["producto_existencia"]:
-            existencia = 0
-        else:
-            existencia = d["producto_existencia"]
+            if not d["producto_costo"]:
+                costo = 0
+            else:
+                costo = d["producto_costo"]
 
-        if not d["producto_costo"]:
-            costo = 0
-        else:
-            costo = d["producto_costo"]
-
-        if not d["producto_precio"]:
-            precio = 0
-        else:
-            precio = d["producto_precio"]
-        Import_Imventario.objects.get_or_create(id=i,
-                                                razon_social=d["razon_social"],
-                                                producto_codigo=d["producto_codigo"],
-                                                producto_serie=d["producto_serie"],
-                                                producto_nombre=d["producto_nombre"],
-                                                producto_existencia=existencia,
-                                                producto_costo=costo,
-                                                producto_precio=precio,
-                                                producto_marca=d["producto_marca"],
-                                                producto_categoria=d["producto_categoria"],
-                                                producto_medida=d["producto_medida"],
-                                                bodega=d["bodega"])
+            if not d["producto_precio"]:
+                precio = 0
+            else:
+                precio = d["producto_precio"]
+            Import_Imventario.objects.get_or_create(id=i,
+                                                    razon_social=d["razon_social"],
+                                                    producto_codigo=d["producto_codigo"],
+                                                    producto_serie=d["producto_serie"],
+                                                    producto_nombre=d["producto_nombre"],
+                                                    producto_existencia=existencia,
+                                                    producto_costo=costo,
+                                                    producto_precio=precio,
+                                                    producto_marca=d["producto_marca"],
+                                                    producto_categoria=d["producto_categoria"],
+                                                    producto_medida=d["producto_medida"],
+                                                    bodega=d["bodega"])
 
 
 def actualizar_cliente():
@@ -75,18 +75,18 @@ def actualizar_cliente():
     assert response.status_code == 200
     json_data = response.json()
     i = 321
-
-    for d in json_data:
-        i += 1
-        if d["identificacion"]:
-            Import.objects.get_or_create(id=i,
-                                         razon_social=d["razon_social"],
-                                         numero_ruc=d["numero_ruc"],
-                                         nombre=d["nombre"],
-                                         identificacion=d["identificacion"],
-                                         telefono=d["telefono"],
-                                         direccion=d["direccion"],
-                                         contacto=d["contacto"])
+    if json_data:
+        for d in json_data:
+            i += 1
+            if d["identificacion"]:
+                Import.objects.get_or_create(id=i,
+                                             razon_social=d["razon_social"],
+                                             numero_ruc=d["numero_ruc"],
+                                             nombre=d["nombre"],
+                                             identificacion=d["identificacion"],
+                                             telefono=d["telefono"],
+                                             direccion=d["direccion"],
+                                             contacto=d["contacto"])
 
 
 @csrf_exempt
